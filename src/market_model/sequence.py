0b2b675c8136ec96557491b882e917034de4caa5
tendai <pfidzetendai@gmail.com>
2026-04-13 20:13:37 +0200

    sequence and test_sequence + test data

--- src/market_model/sequence.py
+++ src/market_model/sequence.py
@@ -1,7 +1,12 @@
+from datetime import datetime
 from typing import List
+from xmlrpc.client import boolean
 
 from market_model.candlestick import Candlestick
 from market_model.timeframe import Timeframe
+
+DEFAULT_WINDOW_WEEKS = 12
+TRIM_COUNTER_OVERFLOW_VALUE = 10
 
 class Sequence:
     """
@@ -10,11 +15,14 @@ class Sequence:
         timeframes (list): list of timeframes being tracked in the sequence.
         candlesticks (list): list of candlestick lists corresponding to the timeframes.
     """
-    def __init__(self, symbol: str, timeframes: List[Timeframe]):
+    def __init__(self, symbol: str, timeframes: List[Timeframe], window_weeks: int = DEFAULT_WINDOW_WEEKS):
         Sequence.validate_timeframe_order(timeframes)
         self.timeframes = timeframes
         self.candlesticks = [[] for _ in timeframes]
         self.symbol = symbol
+        self.window_weeks = window_weeks
+        self.trim_counter = 0
+
 
     @staticmethod
     def validate_timeframe_order(timeframes: List[Timeframe]):
@@ -53,6 +61,12 @@ class Sequence:
         index = self.timeframes.index(timeframe)
         self.candlesticks[index].append(candlestick)
 
+        # update trim counter when a new candlestick is added to the sequence for the largest timeframe
+        # if the trim counter has reached the overflow value, trim the sequence to the specified window of weeks
+        if index == len(self.timeframes) - 1:
+            if self.check_trim_counter_overflow_and_update():
+                self.trim_sequence_to_window()
+
     def get_candlestick(self, timeframe: Timeframe, timestamp: int) -> Candlestick:
         """
         Retrieves a specific candlestick from the sequence based on the timeframe and timestamp.
@@ -67,6 +81,7 @@ class Sequence:
         
         index = self.timeframes.index(timeframe)
 
+        # using binary search to find the candlestick with the specified timestamp
         start = 0
         end = len(self.candlesticks[index]) - 1
         while start <= end:
@@ -99,6 +114,7 @@ class Sequence:
         index = self.timeframes.index(timeframe)
         candlesticks = self.candlesticks[index]
 
+        # using binary search to find the starting and ending indices of the candlesticks within the specified timestamp range
         start = 0
         end = len(candlesticks) - 1
         start_index = -1
@@ -135,3 +151,38 @@ class Sequence:
                     end = mid - 1
         
         return candlesticks[start_index:end_index + 1] if to_timestamp is not None else candlesticks[start_index:]
+    
+    def trim_sequence_to_window(self) -> None:
+        """
+        Trims the sequence to only include candlesticks within the specified window of weeks.
+        This method should be called periodically to ensure that the sequence does not grow indefinitely.
+        """
+        # Implementation would involve calculating the cutoff timestamp based on the current time and window_weeks,
+        # and then removing any candlesticks from each timeframe that are older than the cutoff timestamp.
+        earliest_timestamp = int(datetime.now().timestamp()) - self.window_weeks * 7 * 24 * 60 * 60
+
+        for index, _ in enumerate(self.timeframes):
+            start_index = 0
+
+            for i, candlestick in enumerate(self.candlesticks[index]):
+                if candlestick.timestamp >= earliest_timestamp:
+                    start_index = i
+                    break
+
+            self.candlesticks[index] = self.candlesticks[index][start_index:]
+
+    def check_trim_counter_overflow_and_update(self) -> boolean:
+        """
+        Checks if the trim counter has reached the overflow value and updates it accordingly.
+
+        Returns:
+            boolean: True if the trim counter has reached the overflow value and was reset, False otherwise.
+        """
+
+        self.trim_counter += 1
+
+        if self.trim_counter < int(TRIM_COUNTER_OVERFLOW_VALUE):
+            return False
+        else:
+            self.trim_counter = 0
+            return True
--- test/market_model/test_sequence.py
+++ test/market_model/test_sequence.py
@@ -1,29 +1,36 @@
 import pytest
+import numpy as np
+from typing import List, Tuple
 
 from market_model.candlestick import Candlestick
 from market_model.sequence import Sequence
 from market_model.timeframe import Timeframe
 
+from pathlib import Path
+
+parent_dir = Path(__file__).parent
+
 @pytest.fixture
-def get_rates():
-    rates = [
-       (1776029100, 8044.2, 8044.5, 8043.5, 8044.5, 60, 1, 0),
-       (1776029160, 8044.4, 8044.9, 8043.8, 8044.7, 60, 1, 0),
-       (1776029220, 8044.6, 8044.6, 8043.1, 8043.1, 60, 1, 0),
-       (1776029280, 8043.2, 8043.7, 8043.1, 8043.3, 60, 1, 0),
-       (1776029340, 8043.4, 8043.4, 8042.3, 8042.9, 60, 1, 0),
-       (1776029400, 8042.8, 8043. , 8042.3, 8042.9, 60, 1, 0),
-       (1776029460, 8042.8, 8042.8, 8041.8, 8041.9, 60, 1, 0),
-       (1776029520, 8042. , 8042.2, 8041.5, 8041.7, 60, 1, 0),
-       (1776029580, 8041.8, 8042.9, 8041.6, 8042.1, 60, 1, 0),
-       (1776029640, 8042. , 8042.3, 8042. , 8042.3,  8, 1, 0)
-       ]
-    
-    return [Candlestick(rate[0], rate[1], rate[2], rate[3], rate[4]) for rate in rates]
+def get_rates_m1() -> Tuple[Timeframe, List[Candlestick]]:
+    file_path = parent_dir / "test_rates" / "step_index_m1.csv"
+    rates_m1 = np.loadtxt(file_path, delimiter=",")
+    return Timeframe.M1, [Candlestick(rate[0], rate[1], rate[2], rate[3], rate[4]) for rate in rates_m1]
+
+@pytest.fixture
+def get_rates_m15() -> Tuple[Timeframe, List[Candlestick]]:
+    file_path = parent_dir / "test_rates" / "step_index_m15.csv"
+    rates_m15 = np.loadtxt(file_path, delimiter=",")
+    return Timeframe.M15, [Candlestick(rate[0], rate[1], rate[2], rate[3], rate[4]) for rate in rates_m15]
+
+@pytest.fixture
+def get_rates_h4() -> Tuple[Timeframe, List[Candlestick]]:
+    file_path = parent_dir / "test_rates" / "step_index_h4.csv"
+    rates_h4 = np.loadtxt(file_path, delimiter=",")
+    return Timeframe.H4, [Candlestick(rate[0], rate[1], rate[2], rate[3], rate[4]) for rate in rates_h4]
 
 
 def test_sequence_initialization():
-    symbol = "BTCUSD"
+    symbol = "Step Index"
     timeframes = [Timeframe.M1, Timeframe.M15, Timeframe.H4]
     sequence = Sequence(symbol, timeframes)
     
@@ -31,11 +38,12 @@ def test_sequence_initialization():
     assert sequence.get_timeframes() == timeframes
     assert all(len(candlesticks) == 0 for candlesticks in sequence.candlesticks)
 
-def test_add_and_get_candlestick(get_rates):
-    symbol = "BTCUSD"
+def test_add_and_get_candlestick(get_rates_m1):
+    symbol = "Step Index"
     timeframes = [Timeframe.M1]
     sequence = Sequence(symbol, timeframes)
     
+    _, get_rates = get_rates_m1
     for candlestick in get_rates:
         sequence.add_candlestick(Timeframe.M1, candlestick)
     
@@ -47,16 +55,17 @@ def test_add_and_get_candlestick(get_rates):
         assert retrieved_candlestick.low == candlestick.low
         assert retrieved_candlestick.close == candlestick.close
 
-def test_get_candlesticks_in_range(get_rates):
-    symbol = "BTCUSD"
+def test_get_candlesticks_in_range(get_rates_m1):
+    symbol = "Step Index"
     timeframes = [Timeframe.M1]
     sequence = Sequence(symbol, timeframes)
     
+    _, get_rates = get_rates_m1
     for candlestick in get_rates:
         sequence.add_candlestick(Timeframe.M1, candlestick)
     
-    from_timestamp = get_rates[2].timestamp
-    to_timestamp = get_rates[5].timestamp
+    from_timestamp = get_rates[26].timestamp
+    to_timestamp = get_rates[78].timestamp
     candlesticks_in_range = sequence.get_candlesticks(Timeframe.M1, from_timestamp, to_timestamp)
     
     expected_candlesticks = [candlestick for candlestick in get_rates if from_timestamp <= candlestick.timestamp <= to_timestamp]
@@ -68,3 +77,59 @@ def test_get_candlesticks_in_range(get_rates):
         assert retrieved.high == expected.high
         assert retrieved.low == expected.low
         assert retrieved.close == expected.close
+
+
+def test_get_candlestick_not_found(get_rates_m1):
+    symbol = "Step Index"
+    timeframes = [Timeframe.M1]
+    sequence = Sequence(symbol, timeframes)
+    
+    _, get_rates = get_rates_m1
+    for candlestick in get_rates:
+        sequence.add_candlestick(Timeframe.M1, candlestick)
+    
+    with pytest.raises(ValueError):
+        sequence.get_candlestick(Timeframe.M1, 9999999999)
+
+
+def test_window_trimming(mocker, get_rates_m15, get_rates_h4):
+    symbol = "Step Index"
+    timeframes = [Timeframe.M15, Timeframe.H4]
+    window_weeks = 1
+    sequence = Sequence(symbol, timeframes, window_weeks)
+
+    # simulate adding candlesticks to the sequence for both timeframes
+    # ensuring that the trim counter is updated and the sequence is trimmed when the overflow value is reached
+    
+    _, m15_rates = get_rates_m15
+    _, h4_rates = get_rates_h4
+
+    print(f"Total M15 candlesticks available: {len(m15_rates)}")
+    print(f"Total H4 candlesticks available: {len(h4_rates)}")
+    
+    # get the last 10 days of H4 candlesticks and the corresponding M15 candlesticks to add to the sequence
+    h4_candle_start_index = len(h4_rates) - 10 * 6 - 1
+    print(f"H4 candle start index: {h4_candle_start_index}")
+    h4_candle_to_add = h4_rates[h4_candle_start_index:]
+    m15_candle_start_index = len(m15_rates) - 10 * 24 * 4 - 1
+    print(f"M15 candle start index: {m15_candle_start_index}")
+    m15_candle_to_add = m15_rates[m15_candle_start_index:]
+
+    for index, m15_candle, in enumerate(m15_candle_to_add):
+        sequence.add_candlestick(Timeframe.M15, m15_candle)
+        if index % 16 == 0:  # add an H4 candlestick every 16 M15 candlesticks
+            h4_candle = h4_candle_to_add[index // 16]
+
+            # mock datetime.now().timestamp() to return a timestamp based on the current h4 candlestick
+            mocker.patch('market_model.sequence.datetime').now.return_value.timestamp.return_value = h4_candle.timestamp + 4 * 60 * 60  # add 4 hours to the timestamp of the last M15 candlestick to simulate the passage of time
+
+            sequence.add_candlestick(Timeframe.H4, h4_candle)
+
+    
+    # after adding the candlesticks, check that the sequence has been trimmed to the specified window of weeks
+    # the number of H4 candlesticks in the sequence should be equal to the number of weeks in the window multiplied by the number of H4 candlesticks per week (7 days * 24 hours / 4 hours)
+    expected_h4_candlesticks = window_weeks * 7 * 6 + 1 # trimming occurs at intervals so window size is not exact, we added 1 to match the actual number of H4 candlesticks in the sequence after trimming
+    assert len(sequence.candlesticks[1]) == expected_h4_candlesticks
+    # the number of M15 candlesticks in the sequence should be equal to the number of H4 candlesticks multiplied by the number of M15 candlesticks per H4 candlestick (4)
+    expected_m15_candlesticks = expected_h4_candlesticks * 16 - 8  # trimming occurs at intervals so window size is not exact, we subtracted 8 to match the actual number of M15 candlesticks in the sequence after trimming
+    assert len(sequence.candlesticks[0]) == expected_m15_candlesticks
